e36933a8374f939ecb220782faa38a261e805507
Christophe MULLER <christophe.muller@espi-suisse.ch>
2026-07-04 19:33:10 +0200

    Use and update money

--- source/core/events.gd
+++ source/core/events.gd
@@ -8,8 +8,11 @@ signal selected_level(level_id: int)
 # level starting
 signal start_level
 
-signal summit_reached(climber_lvl : int)
+signal summit_reached(climber_lvl: int)
 
 signal game_over
 
-signal alpinist_died(climber_lvl : int)
+signal alpinist_died(climber_lvl: int)
+
+signal money_updated(new_value: int)
+signal bought_resource(price: int)
--- source/ui/game_overlay.gd
+++ source/ui/game_overlay.gd
@@ -8,6 +8,9 @@ extends CanvasLayer
 @onready var towers: HBoxContainer = %Towers
 @onready var powers: HBoxContainer = %Powers
 
+func _ready() -> void:
+	Events.money_updated.connect(update_resources_count)
+
 # Allows to have new towers and power added in the level scene
 func apply_current_level(level:BaseLevel):
 	for tower in towers.get_children():
@@ -15,5 +18,12 @@ func apply_current_level(level:BaseLevel):
 	for power in powers.get_children():
 		power.parent_node_for_placing = level
 
+func disable_powers():
+	for power in powers.get_children():
+		power.available = false
+
 func _on_start_level_button_pressed() -> void:
 	Events.start_level.emit()
+
+func update_resources_count(new_value:int):
+	resources_count.text = "X " + str(new_value)
